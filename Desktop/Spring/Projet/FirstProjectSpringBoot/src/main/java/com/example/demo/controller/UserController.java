82af1df8b17909117464dc904ce4f5566279ba66
Seifeddine Ben Salah <49524346+SeifeddineBS@users.noreply.github.com>
2021-10-14 13:28:33 +0100

    Controller Seif

--- Desktop/Spring/Projet/FirstProjectSpringBoot/src/main/java/com/example/demo/controller/UserController.java
+++ Desktop/Spring/Projet/FirstProjectSpringBoot/src/main/java/com/example/demo/controller/UserController.java
@@ -4,9 +4,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.demo.entity.Address;
 import com.example.demo.entity.User;
+import com.example.demo.service.Location;
 import com.example.demo.service.UserService;
+
+import java.net.SocketException;
 import java.util.List;
+
+
 
 @RestController
 public class UserController {
@@ -20,11 +27,14 @@ public class UserController {
 		return userService.getUsers();
 	}
 	@PostMapping("addUser")
-	public User addAdmin (@RequestBody User user)
+	public User addAdmin (@RequestBody User user) throws SocketException
 	{
 		
 		return userService.addUser(user);
 		
 	}
+	
+	
+
 	
 }
