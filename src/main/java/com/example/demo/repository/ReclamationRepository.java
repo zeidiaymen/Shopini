9d06ddc12c8a92e080963aab5aea1bca9da0f545
bahaeddinesaid <bahaeddine.said@esprit.tn>
2021-12-14 12:04:10 +0100

    final commit

--- src/main/java/com/example/demo/repository/ReclamationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.Reclamation;
-
-public interface ReclamationRepository extends JpaRepository<Reclamation, Integer>{
-
-}
